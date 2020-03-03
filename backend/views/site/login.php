2df25798f400fba13490cfbc88bb0795460ab5c3
Eugene Storm <storm.eugene@yandex.ru>
2020-03-03 16:01:18 +0500

    admin panel

--- backend/views/site/login.php
+++ backend/views/site/login.php
@@ -8,9 +8,58 @@ use yii\helpers\Html;
 use yii\bootstrap\ActiveForm;
 
 $this->title = 'Login';
-$this->params['breadcrumbs'][] = $this->title;
+//$this->params['breadcrumbs'][] = $this->title;
+$fieldOptions = [
+	'options' => ['class' => 'form-group'],
+	'inputTemplate' => '{input}',
+];
 ?>
-<div class="site-login">
+
+<div class="login-box">
+    <div class="text-center">
+	<h1 class="h2">Административная панель</h1>
+    </div>
+    
+    <?php $form = ActiveForm::begin([
+		'options' => ['class' => 'user'],
+		'enableClientValidation' => false,
+		'fieldConfig' => [
+			'template' => "{input}\n{error}",
+			'errorOptions' => [
+				'class' => 'text-danger small text-center',
+				'style' => 'margin-top: 6px;',
+			],
+		],
+	]) ?>
+    
+    <?= $form->field($model, 'username', $fieldOptions)
+		->label(false)
+		->textInput([
+			'class' => 'form-control form-control-user',
+			'placeholder' => 'Имя',
+		])
+	?>
+    
+    <?= $form->field($model, 'password', $fieldOptions)
+		->label(false)
+		->textInput([
+			'type' => 'password',
+			'class' => 'form-control form-control-user',
+			'placeholder' => 'Пароль',
+		])
+	?>
+
+    <?= $form->field($model, 'rememberMe', $fieldOptions)
+		->checkbox()
+	?>
+
+	<hr>
+
+    <?= Html::submitButton('Войти', ['class' => 'btn btn-primary btn-user btn-block', 'name' => 'login-button']) ?>
+
+    <?php ActiveForm::end() ?>
+    
+<!--<div class="site-login">
     <h1><?= Html::encode($this->title) ?></h1>
 
     <p>Please fill out the following fields to login:</p>
@@ -32,4 +81,5 @@ $this->params['breadcrumbs'][] = $this->title;
             <?php ActiveForm::end(); ?>
         </div>
     </div>
+</div>-->
 </div>
